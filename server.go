25f5d2d369dab38001f56f44149e71dc831724f6
AB-MU <abdelrahmanmustafa00@gmail.com>
2020-05-02 15:09:58 +0200

    update server with cors origin enable

--- server.go
+++ server.go
@@ -4,9 +4,22 @@ import (
 	"net/http"
 
 	"./routes"
+	"github.com/rs/cors"
 )
 
 func main() {
 
-	http.ListenAndServe(":3000", routes.R)
+	// routes.R.GlobalOPTIONS = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 	if r.Header.Get("Access-Control-Request-Method") != "" {
+	// 		// Set CORS headers
+	// 		header := w.Header()
+	// 		header.Set("Access-Control-Allow-Methods", r.Header.Get("Allow"))
+	// 		header.Set("Access-Control-Allow-Origin", "*")
+	// 	}
+
+	// 	// Adjust status code to 204
+	// 	w.WriteHeader(http.StatusNoContent)
+	// })
+	handler := cors.Default().Handler(routes.R)
+	http.ListenAndServe(":3000", handler)
 }
