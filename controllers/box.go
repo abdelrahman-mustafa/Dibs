b122a912a860b03204a553608bc24dd936cbf5d1
AB-MU <abdelrahmanmustafa00@gmail.com>
2020-05-12 13:32:32 +0200

    update box

--- controllers/box.go
+++ controllers/box.go
@@ -66,7 +66,7 @@ func (ad BoxController) UpdateBox(w http.ResponseWriter, r *http.Request, p http
 	Box := models.Box{}
 	//prase json  of body and attach to admoin struct
 	json.NewDecoder(r.Body).Decode(&Box)
-	oid := bson.ObjectIdHex(p.ByName("id"))
+	oid := p.ByName("id")
 
 	// write struct of admni to DB
 	ad.session.DB("dibs").C("Boxes").UpdateId(oid, Box)
