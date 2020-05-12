b61a399094ab142cf28efda49ed358e6e1674c63
AB-MU <abdelrahmanmustafa00@gmail.com>
2020-05-12 15:04:43 +0200

    update box

--- models/box.go
+++ models/box.go
@@ -7,21 +7,21 @@ import (
 type (
 	// Box represents the structure of our resource
 	Box struct {
-		ID             bson.ObjectId `json:"id,omitempty" bson:"_id" `
-		Name           string        `json:"name,omitempty" bson:"name"`
-		Username       string        `json:"username,omitempty" bson:"username"`
-		Password       string        `json:"password,omitempty" bson:"password"`
-		AvailableBoxes int           `json:"availableBoxes,omitempty" bson:"availableBoxes"`
-		Long           float64       `json:"long,omitempty" bson:"long"`
-		Lat            float64       `json:"lat,omitempty" bson:"lat"`
-		From           int           `json:"from,omitempty" bson:"from"`
-		To             int           `json:"to,omitempty" bson:"to"`
-		Price          int           `json:"price,omitempty" bson:"price"`
-		Banner         string        `json:"banner,omitempty" bson:"banner"`
-		Logo           string        `json:"logo,omitempty" bson:"logo"`
-		Contact        string        `json:"contact,omitempty" bson:"contact"`
-		Description    string        `json:"description,omitempty" bson:"description"`
-		IsActive       bool          `json:"isActive,omitempty" bson:"isActive"`
+		ID             bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
+		Name           string        `json:"name,omitempty" bson:"name,omitempty"`
+		Username       string        `json:"username,omitempty" bson:"username,omitempty"`
+		Password       string        `json:"password,omitempty" bson:"password,omitempty"`
+		AvailableBoxes int           `json:"availableBoxes,omitempty" bson:"availableBoxes,omitempty"`
+		Long           float64       `json:"long,omitempty" bson:"long,omitempty"`
+		Lat            float64       `json:"lat,omitempty" bson:"lat,omitempty"`
+		From           int           `json:"from,omitempty" bson:"from,omitempty"`
+		To             int           `json:"to,omitempty" bson:"to,omitempty"`
+		Price          int           `json:"price,omitempty" bson:"price,omitempty"`
+		Banner         string        `json:"banner,omitempty" bson:"banner,omitempty"`
+		Logo           string        `json:"logo,omitempty" bson:"logo,omitempty"`
+		Contact        string        `json:"contact,omitempty" bson:"contact,omitempty"`
+		Description    string        `json:"description,omitempty" bson:"description,omitempty"`
+		IsActive       bool          `json:"isActive,omitempty" bson:"isActive,omitempty"`
 	}
 )
 
@@ -44,19 +44,19 @@ type (
 	//UserBox ... UserBox
 	UserBox struct {
 		ID             bson.ObjectId `json:"id,omitempty" bson:"_id" `
-		Name           string        `json:"name,omitempty" bson:"name"`
-		Username       string        `json:"-" bson:"username"`
-		Password       string        `json:"-" bson:"password"`
-		AvailableBoxes int           `json:"availableBoxes,omitempty" bson:"availableBoxes"`
-		Long           string        `json:"long,omitempty" bson:"long"`
-		Lat            string        `json:"lat,omitempty" bson:"lat"`
-		From           string        `json:"from,omitempty" bson:"from"`
-		To             string        `json:"to,omitempty" bson:"to"`
-		Price          int           `json:"price,omitempty" bson:"price"`
-		Banner         string        `json:"banner,omitempty" bson:"banner"`
-		Logo           string        `json:"logo,omitempty" bson:"logo"`
-		Contact        string        `json:"contact,omitempty" bson:"contact"`
-		Description    string        `json:"description,omitempty" bson:"description"`
-		IsActive       bool          `json:"isActive,omitempty" bson:"isActive"`
+		Name           string        `json:"name,omitempty" bson:"name,omitempty"`
+		Username       string        `json:"-" bson:"username,omitempty"`
+		Password       string        `json:"-" bson:"password,omitempty"`
+		AvailableBoxes int           `json:"availableBoxes,omitempty" bson:"availableBoxes,omitempty"`
+		Long           string        `json:"long,omitempty" bson:"long,omitempty"`
+		Lat            string        `json:"lat,omitempty" bson:"lat,omitempty"`
+		From           string        `json:"from,omitempty" bson:"from,omitempty"`
+		To             string        `json:"to,omitempty" bson:"to,omitempty"`
+		Price          int           `json:"price,omitempty" bson:"price,omitempty"`
+		Banner         string        `json:"banner,omitempty" bson:"banner,omitempty"`
+		Logo           string        `json:"logo,omitempty" bson:"logo,omitempty"`
+		Contact        string        `json:"contact,omitempty" bson:"contact,omitempty"`
+		Description    string        `json:"description,omitempty" bson:"description,omitempty"`
+		IsActive       bool          `json:"isActive,omitempty" bson:"isActive,omitempty"`
 	}
 )
