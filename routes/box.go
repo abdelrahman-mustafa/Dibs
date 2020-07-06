a98a9f3fcfe5d8102e5af4b32ed90f8be6a11a0c
AB-MU <abdelrahmanmustafa00@gmail.com>
2020-07-07 00:27:18 +0200

    update

--- routes/box.go
+++ routes/box.go
@@ -12,7 +12,9 @@ func InitBox(R *httprouter.Router, session *mgo.Session) {
 	Box := controllers.NewBoxController(session)
 
 	R.POST("/admin/box", helpers.Authenticate(Box.CreateBox))
-	R.POST("/box/signin", helpers.Authenticate(Box.SignIn))
+	R.POST("/box/signin", Box.SignIn)
+	R.PUT("/box/:id", helpers.Authenticate(Box.UpdateBox))
+
 	R.PUT("/admin/box/:id", helpers.Authenticate(Box.UpdateBox))
 	R.PUT("/admin/box/:id/activation", helpers.Authenticate(Box.UpdateActivationBox))
 	R.GET("/admin/boxes", helpers.Authenticate(Box.GetBoxes))
