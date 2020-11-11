469ed86cd4719d0a54efbf7b351e998b371865dd
AB-MU <abdelrahmanmustafa00@gmail.com>
2020-11-11 18:20:35 +0200

    fix

--- routes/order.go
+++ routes/order.go
@@ -11,7 +11,7 @@ import (
 func InitOrder(R *httprouter.Router, session *mgo.Session) {
 	Order := controllers.NewOrderController(session)
 	R.POST("/order", helpers.Authenticate(Order.CreateOrder))
-	R.GET("/order/list", helpers.Authenticate(Order.GetOrders))
+	R.GET("/order", helpers.Authenticate(Order.GetOrders))
 	R.GET("/order/:id", helpers.Authenticate(Order.GetOrder))
 
 }
